f9d90b8fc2f2df813cc324840f3c105b757f79f3
Jackie <jackie15910@gmail.com>
2023-07-27 15:12:00 -0400

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,21 +1,26 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'vagrant',
+  user: 'labber',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
+const query = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${process.argv[2]}%`, process.argv[3] || 5];
+
+pool.query(query, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
